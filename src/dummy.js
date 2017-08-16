adae6c3a25183617545f20e11d205bdc2aad8c9c
Matt Paul <mattpaul001@icloud.com>
2017-08-16 11:00:04 +0100

    Silly test for circle ci to run

---
+++ src/dummy.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+  return a + b;
+}
+
+const num = add(8,3)
+num
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var expect = require('chai').expect;
+describe('silly test for circle ci', () => {
+  it('should be 3', () => {
+    expect(3).to.equal(3)
+  })
+});
